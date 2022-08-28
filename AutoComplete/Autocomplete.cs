d98d9b1c1b910308d63a60b215d50d3a370d3d14
NemethBalint96 <nemeth.balint1996@gmail.com>
2022-08-28 22:13:04 +0200

    Get matches

--- AutoComplete/Autocomplete.cs
+++ AutoComplete/Autocomplete.cs
@@ -1,13 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace AutoComplete
-{
+namespace AutoComplete;
+
     public class Autocomplete : Trie
     {
         public List<string> GetMatches(string prefix)
         {
-            throw new NotImplementedException();
+        var possibleCompleteWords = new List<string>();
+        if (!IsPrefix(prefix.ToCharArray())) return possibleCompleteWords;
+        var tempNode = GetLastNodeOfPrefix(prefix.ToCharArray());
+        if (tempNode.IsEndOfWord)
+        {
+            possibleCompleteWords.Add(prefix);
         }
+        AddEveryPossibleCompleteWord(tempNode, prefix, possibleCompleteWords);
+
+        return possibleCompleteWords;
+    }
+
+    private void AddEveryPossibleCompleteWord(TrieNode tempNode, string partialWord, List<string> possibleCompleteWords)
+    {
+        foreach (var letter in tempNode.ChildNodes.Keys)
+        {
+            var possibility = partialWord;
+            possibility += letter;
+            if (tempNode.ChildNodes[letter].IsEndOfWord)
+            {
+                possibleCompleteWords.Add(possibility);
+            }
+            AddEveryPossibleCompleteWord(tempNode.ChildNodes[letter], possibility, possibleCompleteWords);
+        }
+    }
+
+    private TrieNode GetLastNodeOfPrefix(char[] chars)
+    {
+        var tempNode = Root;
+        foreach (var c in chars)
+        {
+            if (tempNode.ChildNodes.ContainsKey(c))
+                tempNode = tempNode.ChildNodes[c];
+        }
+
+        return tempNode;
+    }
+
+    private bool IsPrefix(char[] chars)
+    {
+        var tempNode = Root;
+        foreach (var c in chars)
+        {
+            if (!tempNode.ChildNodes.ContainsKey(c))
+                return false;
+
+            tempNode = tempNode.ChildNodes[c];
+        }
+        return true;
     }
 }
